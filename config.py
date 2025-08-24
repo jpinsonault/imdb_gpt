dfa5310883d96ce386715278f0f21bf654e4f7aa
Joe Pinsonault <joe.pinsonault@gmail.com>
2025-08-24 10:42:59 -0700

    change batch

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ project_config = {
     'docker_data_dir_mount': '/app/imdb',
 
     'latent_dim': 256,
-    'batch_size': 2024,
+    'batch_size': 512,
     'learning_rate': 0.0005,
     'weight_decay': 1e-4,
 
